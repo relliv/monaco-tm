b04a21f4ccf631b25f06f69f2eedc88adafbc299
relliv <epsilonelittr@gmail.com>
2024-03-18 02:50:43 +0300

    Apply refactor

--- src/app.ts
+++ src/app.ts
@@ -32,19 +32,23 @@ import { initVimMode } from "monaco-vim"
 ;(window as any).setTheme = setTheme
 
 MonacoEnvironment = {
-  getWorkerUrl: function (moduleId: any, label: String) {
-    // if (label === 'json') {
-    // 	return './json.worker.bundle.js';
-    // }
+  getWorkerUrl: (_moduleId: any, label: String) => {
+    if (label === "json") {
+      return "./json.worker.bundle.js"
+    }
+
     if (label === "css" || label === "scss" || label === "less") {
       return "./css.worker.bundle.js"
     }
+
     if (label === "html" || label === "handlebars" || label === "razor") {
       return "./html.worker.bundle.js"
     }
+
     if (label === "typescript" || label === "javascript") {
       return "./ts.worker.bundle.js"
     }
+
     return "./editor.worker.bundle.js"
   }
 }
