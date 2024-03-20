a7a5b2ceedffcbcfe488972604783b0c9daca55b
relliv <epsilonelittr@gmail.com>
2024-03-20 03:24:19 +0300

    Update pacakge.json content

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 import RegexUtils from "./utils/regex"
 import RegisterUtils from "./utils/register"
 
+// providers
 import { SimpleLanguageInfoProvider } from "./providers/common-providers"
 
+// exports
 export { SimpleLanguageInfoProvider, RegisterUtils, RegexUtils }
